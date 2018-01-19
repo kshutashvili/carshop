f51c8c39c742564f0cf5896640ddc0cf1b014517
phonxis <phonxis@gmail.com>
2018-01-19 21:14:31 +0200

    2nd build

--- build/js/basket.js
+++ build/js/basket.js
@@ -1,4 +1,4 @@
-webpackJsonp([2],{
+webpackJsonp([4],{
 
 /***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
@@ -10287,13 +10287,13 @@ exports.default = function () {
 
 /***/ }),
 
-/***/ 12:
+/***/ 23:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function($) {
 
-__webpack_require__(13);
+__webpack_require__(24);
 
 var _catalogMenu = __webpack_require__(1);
 
@@ -10301,40 +10301,19 @@ var _catalogMenu2 = _interopRequireDefault(_catalogMenu);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var switchProductVIew = function switchProductVIew() {
-    var $blockBtn = $("#block-btn");
-    var $listBtn = $("#list-btn");
-    var $tovarList = $("#tovar-list");
-
-    $listBtn.off("click");
-    $listBtn.on("click", function () {
-        $blockBtn.removeClass("active");
-        $(this).addClass("active");
-        $tovarList.addClass("list-view");
-    });
-
-    $blockBtn.off("click");
-    $blockBtn.on("click", function () {
-        $listBtn.removeClass("active");
-        $tovarList.removeClass("list-view");
-        $(this).addClass("active");
-    });
-};
-
 $(window).on("load", function () {
     (0, _catalogMenu2.default)();
-    switchProductVIew();
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
 
-/***/ 13:
+/***/ 24:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ })
 
-},[12]);
-//# sourceMappingURL=catalog.js.map
+},[23]);
+//# sourceMappingURL=basket.js.map
---
+++ build/js/login.js
@@ -0,0 +1,21 @@
+webpackJsonp([6],{
+
+/***/ 25:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(26);
+
+/***/ }),
+
+/***/ 26:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[25]);
+//# sourceMappingURL=login.js.map
